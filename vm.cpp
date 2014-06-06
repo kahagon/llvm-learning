294520640a5b6e0b8c9b9af914f7780373100717
Keng-ichi Ahagon <k.ahagon@n-3.so>
2014-06-07 03:03:41 +0900

    fix indentation

--- vm.cpp
+++ vm.cpp
@@ -20,28 +20,28 @@ Function* append_test_block_return_int(
   Module* mod, 
   Function* func) 
 {
-	BasicBlock *basic_block = BasicBlock::Create(ctx, "ret_int", func);
-	builder->SetInsertPoint(basic_block);
+  BasicBlock *basic_block = BasicBlock::Create(ctx, "ret_int", func);
+  builder->SetInsertPoint(basic_block);
 
-	// TODO: generate function body
-	int var_int_value = 34;
-	std::string var_name = "i";
+  // TODO: generate function body
+  int var_int_value = 34;
+  std::string var_name = "i";
   Value* var = builder->CreateAlloca(Type::getInt32Ty(ctx),	0, var_name);
-	ValueSymbolTable& function_value_symbol_table 
-		= func->getValueSymbolTable();
+  ValueSymbolTable& function_value_symbol_table 
+    = func->getValueSymbolTable();
 
-	Value* lhs_v;
-	Value* rhs_v;
+  Value* lhs_v;
+  Value* rhs_v;
 
-	// 'lhs_v' is equal to 'var'
+  // 'lhs_v' is equal to 'var'
   lhs_v = function_value_symbol_table.lookup(var_name);
-	rhs_v = ConstantInt::get(
+  rhs_v = ConstantInt::get(
     Type::getInt32Ty(ctx), var_int_value);
-	builder->CreateStore(rhs_v, lhs_v);
+  builder->CreateStore(rhs_v, lhs_v);
 
-	// return i
-	builder->CreateRet(builder->CreateLoad(lhs_v, "var_tmp"));
-	return func;
+  // return i
+  builder->CreateRet(builder->CreateLoad(lhs_v, "var_tmp"));
+  return func;
 }
 
 Function* append_test_block_print_string(
@@ -53,42 +53,41 @@ Function* append_test_block_print_string(
   BasicBlock *block = BasicBlock::Create(ctx, "print_string", func);
   builder->SetInsertPoint(block);
 
-	ValueSymbolTable& symbol_table = func->getValueSymbolTable();
+  ValueSymbolTable& symbol_table = func->getValueSymbolTable();
   StringRef str_ref("Hello, World\n");
   Constant* string = ConstantDataArray::getString(ctx, str_ref);
 }
 
 Function *generate_function_echo(LLVMContext& ctx, IRBuilder<>* builder, Module *mod) {
-	std::vector<Type*> param_types;
+  std::vector<Type*> param_types;
   param_types.push_back(Type::getInt64Ty(ctx));
-	Type* result_type = Type::getVoidTy(ctx);
-	std::string func_name = "echo";
-	FunctionType* func_type 
-		= FunctionType::get(result_type, param_types, false);
-	Function* func = Function::Create(
-		func_type, Function::ExternalLinkage,	func_name, mod);
+  Type* result_type = Type::getVoidTy(ctx);
+  std::string func_name = "echo";
+  FunctionType* func_type 
+    = FunctionType::get(result_type, param_types, false);
+  Function* func = Function::Create(
+    func_type, Function::ExternalLinkage,	func_name, mod);
 
   BasicBlock *block = BasicBlock::Create(ctx, "echo_string", func);
   builder->SetInsertPoint(block);
 
-	ValueSymbolTable& symbol_table = func->getValueSymbolTable();
+  ValueSymbolTable& symbol_table = func->getValueSymbolTable();
   StringRef str_ref("Hello, World\n");
   Constant* string = ConstantDataArray::getString(ctx, str_ref);
 
-
-	return func;
+  return func;
 }
 
 Function *generate_function_main(LLVMContext& ctx, IRBuilder<>* builder, Module *mod) {
-	std::vector<Type*> param_types(0, Type::getVoidTy(ctx));
-	Type* result_type = Type::getInt32Ty(ctx);
-	std::string func_name = "main";
-	FunctionType* func_type 
-		= FunctionType::get(result_type, param_types, false);
-	Function* func = Function::Create(
-		func_type, Function::ExternalLinkage,	func_name, mod);
+  std::vector<Type*> param_types(0, Type::getVoidTy(ctx));
+  Type* result_type = Type::getInt32Ty(ctx);
+  std::string func_name = "main";
+  FunctionType* func_type 
+    = FunctionType::get(result_type, param_types, false);
+  Function* func = Function::Create(
+    func_type, Function::ExternalLinkage,	func_name, mod);
 
-	return func;
+  return func;
 }
 
 void ibm_tutorial(LLVMContext& ctx, IRBuilder<>* builder, Module *mod) {
@@ -108,7 +107,6 @@ void ibm_tutorial(LLVMContext& ctx, IRBuilder<>* builder, Module *mod) {
 
   builder->CreateCall(puts_func, helloWorld);
 
-  
   // struct test_struct { int; char*; }; 
   std::vector<Type *> types;
   types.push_back(Type::getInt64Ty(ctx)); // integer member
@@ -127,15 +125,15 @@ void ibm_tutorial(LLVMContext& ctx, IRBuilder<>* builder, Module *mod) {
 }
 
 void print_module(Module* mod, std::string file_name) {
-	PassManager pass_manager;
-	std::string error;
+  PassManager pass_manager;
+  std::string error;
   //sys::fs::OpenFlags flags = sys::fs::OpenFlags::F_RW;
-	raw_fd_ostream raw_stream(file_name.c_str(), error, sys::fs::F_RW);
+  raw_fd_ostream raw_stream(file_name.c_str(), error, sys::fs::F_RW);
 
-	pass_manager.add(createPromoteMemoryToRegisterPass());
-	pass_manager.add(createPrintModulePass(raw_stream));
-	pass_manager.run(*mod);
-	raw_stream.close();
+  pass_manager.add(createPromoteMemoryToRegisterPass());
+  pass_manager.add(createPrintModulePass(raw_stream));
+  pass_manager.run(*mod);
+  raw_stream.close();
 }
 
 /*
@@ -145,19 +143,19 @@ void print_module(Module* mod, std::string file_name) {
  * }
  */
 int main(int argc, char *argv[]) {
-	std::string file_name = "main.ll";
+  std::string file_name = "main.ll";
   LLVMContext& ctx = getGlobalContext();
-	IRBuilder<> *builder = new IRBuilder<>(ctx);
-	Module *mod = new Module("llvm-test", ctx);
-	//Function* func = generate_function_main(ctx, builder, mod);
+  IRBuilder<> *builder = new IRBuilder<>(ctx);
+  Module *mod = new Module("llvm-test", ctx);
+  //Function* func = generate_function_main(ctx, builder, mod);
 
-	ibm_tutorial(ctx, builder, mod);
+  ibm_tutorial(ctx, builder, mod);
   //mod->dump();
 
   //append_test_block_print_string(ctx, builder, mod, func);
   //append_test_block_return_int(ctx, builder, mod, func);
 
-	print_module(mod, file_name);
+  print_module(mod, file_name);
 
-	return 0;
+  return 0;
 }
